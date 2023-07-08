a3d08abd54a7ec25c64d4e221ceba6c05a1bd785
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-07-08 14:25:37 +0300

    Run build

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,23 +10,23 @@ function Gallery() {
   return (
     <div className="container1">
   <div className="card">
-    <img src={tomato}/>
+    <img src={tomato} alt="..."/>
     <div className="card__head bg-lime-400">Tomato Farming</div>
   </div>
   <div className="card">
-    <img src={bellpepper}/>
+    <img src={bellpepper} alt="..."/>
     <div className="card__head bg-lime-400">Bell Pepper Farming</div>
   </div>
   <div className="card">
-    <img src={potato}/>
+    <img src={potato} alt="..."/>
     <div className="card__head bg-lime-400">Potato Farming</div>
   </div>
   <div className="card">
-    <img src={fruits}/>
+    <img src={fruits} alt="..."/>
     <div className="card__head bg-lime-400">Exotic Fruits</div>
   </div>
   <div className="card">
-    <img src={peppers}/>
+    <img src={peppers} alt="..."/>
     <div className="card__head bg-lime-400">Pepper Farming</div>
   </div>
 </div>
